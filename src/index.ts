6197619e84c91342f9cbd9a86869e2bf995f181d
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2025-08-31 18:49:15 +0800

    feat: added jwt
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import jwt from "@elysiajs/jwt";
 import { Elysia } from "elysia";
+import { configEnv } from "./config/config";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(
+		jwt({
+			name: 'jwt',
+			secret: configEnv.JWT_SECRET,
+			iss: 'Merpati-CBT',
+			aud: 'Authentication',
+			exp: '8h',
+		}),
+	);
+
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
